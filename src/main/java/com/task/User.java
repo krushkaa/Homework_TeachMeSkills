933e8a42c10efa517f715e611299c61a6ca49f0f
Kirill Palianitsa <kirillpal22@gmail.com>
2024-07-01 20:47:18 +0300

    SpringMVC-HW-38

---
+++ src/main/java/com/task/User.java
@@ -0,0 +1,33 @@
+package com.task;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+
+}
